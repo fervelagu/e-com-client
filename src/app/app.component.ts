b41dad9c0da2d29317a9b7d0162dba348da1e5c6
Fernanda velazquez <fervlazq@gmail.com>
2018-10-18 17:03:59 -0500

    bootstrap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecomclient';
+  title = 'My E-Commerce';
+  user: any = {}
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ){}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from "@angular/router"
+import { routes} from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
